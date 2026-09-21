4ec1cf6996bc702591f2adab5136ce7b6911de6f
charankumardigitalraiz-max <charankumardigitalraiz@gmail.com>
2026-09-21 13:37:15 +0530

    dsdas

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -55,12 +55,14 @@ export const metadata: Metadata = {
   ],
   icons: {
     icon: [
+      { url: "/favicon.png", type: "image/png" },
       { url: "/favicon.ico", sizes: "any" },
       { url: "/icon.webp", type: "image/png", sizes: "32x32" },
       { url: "/icon-192.webp", type: "image/png", sizes: "192x192" },
     ],
-    shortcut: "/favicon.ico",
+    shortcut: "/favicon.png",
     apple: [
+      { url: "/favicon.png", sizes: "180x180", type: "image/png" },
       { url: "/apple-touch-icon.webp", sizes: "180x180", type: "image/png" },
     ],
   },
